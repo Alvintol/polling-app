043d144c2bcb73e9871e0fb0cb38f0ce934b27c7
Alvin <alvin.tol@hotmail.com>
2022-09-19 12:13:52 -0600

    adds poll config to settings

--- polling/settings.py
+++ polling/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.app.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
