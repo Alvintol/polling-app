7d34e81c413125fa0c33b3797b0f2d5a2af76c64
Alvin <alvin.tol@hotmail.com>
2022-09-21 10:53:21 -0600

    adds results button to html

--- polls/views.py
+++ polls/views.py
@@ -20,6 +20,6 @@ def detail(request, question_id):
         raise Http404('Question does not exist')
     return render(request, 'polls/results.html', {'question': question})
 
-def result(request, question_id):
+def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls.results.html', {'question': question})
