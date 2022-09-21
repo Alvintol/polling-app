eae4b5d560546ed502cbf4723299c049197f3a42
Alvin <alvin.tol@hotmail.com>
2022-09-21 10:51:07 -0600

    adds result method in views

--- polls/views.py
+++ polls/views.py
@@ -19,3 +19,7 @@ def detail(request, question_id):
     except Question.DoesNotExist:
         raise Http404('Question does not exist')
     return render(request, 'polls/results.html', {'question': question})
+
+def result(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls.results.html', {'question': question})
