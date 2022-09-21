29ca623e751fa3f57005280fad689c9a47053e62
Alvin <alvin.tol@hotmail.com>
2022-09-21 12:35:33 -0600

    corrects spelling errors and bootstrap classes for votes

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
 from .models import Question, Choice
 
 
@@ -22,12 +23,12 @@ def detail(request, question_id):
 
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls.results.html', {'question': question})
+    return render(request, 'polls/results.html', {'question': question})
 
 def vote(request, question_id):
-    question = get_object_404(Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST('choice'))
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             'question': question,
@@ -37,5 +38,5 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id)))
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
         
